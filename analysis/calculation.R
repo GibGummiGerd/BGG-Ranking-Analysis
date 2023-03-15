7551e613242dde4e71ade950db561da1cc2b3cc5
GibGummiGerd <christian.giel@web.de>
2023-03-16 00:54:39 +0100

    move out date calculation, adjust summing up

--- analysis/calculation.R
+++ analysis/calculation.R
@@ -2,6 +2,7 @@ library(tidyverse)
 source("const.R")
 source("ranking.R")
 source("files.R")
+source("date.R")
 
 
 #
@@ -172,11 +173,11 @@ calc_ratings_per_month <- function(data,
   )
   ratings_per_month <- ratings_per_month %>% left_join(cum_avg_rating_no_comment, by = c("year", "month"))
 
-  release <- get_release_year_and_month(ratings_per_month, year_official)
+  release <- calculate_date_available_and_first_rating(ratings_per_month, year_official)
 
   ratings_per_month <- ratings_per_month %>%
     mutate(.,
-      month_after_release = 12 * (year - release$start_year) + (month - release$start_month) # add + 1 if first month should be 1
+      month_after_release = 12 * (year - release$year_available) + (month - release$month_available) # add + 1 if first month should be 1
     )
 
   return(ratings_per_month)
@@ -219,99 +220,6 @@ calc_cum_avg_rating <- function(avg_rating_column,
 }
 
 
-#
-get_release_year_and_month <- function(ratings_per_month, year_official) {
-  start_month <- 5
-  start_year <- 2001
-  prior_month <- NA
-  prior_year <- NA
-  valid_months <- 0
-  year_reached <- FALSE
-  current_months_in_a_row <- 0
-  start_is_first <- TRUE
-
-  vector_no_ratings <- ratings_per_month$no_ratings
-  vector_years <- ratings_per_month$year
-  vector_months <- ratings_per_month$month
-
-  if (year_official <= 2000) {
-    months_in_a_row <- 1
-    minimum_ratings_per_month <- 1
-  } else if (year_official <= 2004) {
-    months_in_a_row <- 3
-    minimum_ratings_per_month <- 3
-  } else if (year_official <= 2009) {
-    months_in_a_row <- 3
-    minimum_ratings_per_month <- 3
-  } else if (year_official <= 2016) {
-    months_in_a_row <- 6
-    minimum_ratings_per_month <- 10
-  } else {
-    months_in_a_row <- 6
-    minimum_ratings_per_month <- 10
-  }
-
-  for (key in 1:length(vector_years)) {
-    if (year_reached == FALSE) {
-      if (vector_years[key] >= year_official) {
-        year_reached <- TRUE
-      } else {
-        start_is_first <- FALSE
-        prior_month <- vector_months[key]
-        prior_year <- vector_years[key]
-        next
-      }
-    }
-
-
-    if (vector_no_ratings[key] >= minimum_ratings_per_month) {
-      current_months_in_a_row <- current_months_in_a_row + 1
-    } else {
-      current_months_in_a_row <- 0
-      start_is_first <- FALSE
-      prior_month <- vector_months[key]
-      prior_year <- vector_years[key]
-      next
-    }
-
-    if (current_months_in_a_row == 1) {
-      start_month <- vector_months[key]
-      start_year <- vector_years[key]
-      # Check if a month was skipped, as in had no ratings and is not in list
-    } else if (
-      # previous month is not month - 1
-      ((start_month + current_months_in_a_row - 1) %% 12 != (vector_months[key]) %% 12) |
-        # month not 1, but previous month in different year
-        (vector_months[key] != 1 & vector_years[key] != vector_years[key - 1]) |
-        # month is 1, but year is not previous year
-        (vector_years[key] == 1 & vector_years[key] - 1 != vector_years[key - 1])
-    ) {
-      current_months_in_a_row <- 0
-      start_is_first <- FALSE
-      prior_month <- vector_months[key]
-      prior_year <- vector_years[key]
-      next
-    }
-
-    if (current_months_in_a_row == months_in_a_row) {
-      return(list(
-        "start_month" = start_month,
-        "start_year" = start_year,
-        "start_is_first" = start_is_first,
-        "prior_month" = prior_month,
-        "prior_year" = prior_year
-      ))
-    }
-  }
-
-  return(list(
-    "start_month" = start_month,
-    "start_year" = start_year,
-    "start_is_first" = start_is_first,
-    "prior_month" = prior_month,
-    "prior_year" = prior_year
-  ))
-}
 
 
 create_and_save_monthly_data <- function(game_rank, game_id, game_name, release_year, ranking_year, ranking_month) {
@@ -336,23 +244,56 @@ get_monthly_data <- function() {
   data <- load_csv_with_id(game_id, CSV_GAMES_MONTHLY_FOLDER)
 }
 
-# Sums up all the last months columns
+# Sums up basic information about the cumulated information from all games
+# number and average rating of owned, not owned, prev owned, comment, no comment
 sum_up_last_months <- function(all_last_month_stats) {
-  summed_up <- all_last_month_stats %>%
-    summarize(
-      no_ratings = sum(cum_no_ratings),
-      avg_rating = (sum(cum_no_ratings * cum_avg_rating)) / sum(cum_no_ratings),
-      no_ratings_owned = sum(cum_no_ratings_owned),
-      avg_rating_owned = (sum(cum_no_ratings_owned * cum_avg_rating_owned)) / sum(cum_no_ratings_owned),
-      no_ratings_not_owned = sum(cum_no_ratings_not_owned),
-      avg_rating_not_owned = (sum(cum_no_ratings_not_owned * cum_avg_rating_not_owned)) / sum(cum_no_ratings_not_owned),
-      no_rating_prev_owneds = sum(cum_no_ratings_prev_owned),
-      avg_rating_prev_owned = (sum(cum_no_ratings_prev_owned * cum_avg_rating_prev_owned)) / sum(cum_no_ratings_prev_owned),
-      no_ratings_comment = sum(cum_no_ratings_comment),
-      avg_rating_comment = (sum(cum_no_ratings_comment * cum_avg_rating_comment)) / sum(cum_no_ratings_comment),
-      no_ratings_no_comment = sum(cum_no_ratings_no_comment),
-      avg_rating_no_comment = (sum(cum_no_ratings_no_comment * cum_avg_rating_no_comment)) / sum(cum_no_ratings_no_comment)
-    )
+  summed_up <- data.frame(matrix(ncol = 4, nrow = 6))
+
+
+  no_ratings <- sum(all_last_month_stats["cum_no_ratings"])
+  no_ratings_owned <- sum(all_last_month_stats["cum_no_ratings_owned"])
+  no_ratings_not_owned <- sum(all_last_month_stats["cum_no_ratings_not_owned"])
+  no_ratings_prev_owned <- sum(all_last_month_stats["cum_no_ratings_prev_owned"])
+  no_ratings_comment <- sum(all_last_month_stats["cum_no_ratings_comment"])
+  no_ratings_no_comment <- sum(all_last_month_stats["cum_no_ratings_no_comment"])
+
+  name_vector <- c(
+    "All ratings",
+    "Game owned",
+    "Game not owned",
+    "Game previously owned",
+    "Rating with comment",
+    "Rating with no comment"
+  )
+
+  no_ratings_vector <- c(
+    no_ratings,
+    no_ratings_owned,
+    no_ratings_not_owned,
+    no_ratings_prev_owned,
+    no_ratings_comment,
+    no_ratings_no_comment
+  )
+  avg_rating_vector <- c(
+    (sum(all_last_month_stats["cum_no_ratings"] * all_last_month_stats["cum_avg_rating"])) / sum(all_last_month_stats["cum_no_ratings"]),
+    (sum(all_last_month_stats["cum_no_ratings_owned"] * all_last_month_stats["cum_avg_rating_owned"])) / sum(all_last_month_stats["cum_no_ratings_owned"]),
+    (sum(all_last_month_stats["cum_no_ratings_not_owned"] * all_last_month_stats["cum_avg_rating_not_owned"])) / sum(all_last_month_stats["cum_no_ratings_not_owned"]),
+    (sum(all_last_month_stats["cum_no_ratings_prev_owned"] * all_last_month_stats["cum_avg_rating_prev_owned"])) / sum(all_last_month_stats["cum_no_ratings_prev_owned"]),
+    (sum(all_last_month_stats["cum_no_ratings_comment"] * all_last_month_stats["cum_avg_rating_comment"])) / sum(all_last_month_stats["cum_no_ratings_comment"]),
+    (sum(all_last_month_stats["cum_no_ratings_no_comment"] * all_last_month_stats["cum_avg_rating_no_comment"])) / sum(all_last_month_stats["cum_no_ratings_no_comment"])
+  )
+  percentage_vector <- c(
+    (no_ratings / no_ratings) * 100,
+    (no_ratings_owned / no_ratings) * 100,
+    (no_ratings_not_owned / no_ratings) * 100,
+    (no_ratings_prev_owned / no_ratings) * 100,
+    (no_ratings_comment / no_ratings) * 100,
+    (no_ratings_no_comment / no_ratings) * 100
+  )
+
+  summed_up <- data.frame(name_vector, no_ratings_vector, percentage_vector, avg_rating_vector)
+
+  colnames(summed_up) <- c("type_rating", "no_ratings", "percentage", "avg_rating")
 
   return(summed_up)
 }
@@ -443,7 +384,6 @@ collect_last_months <- function(no_top_games = NUMBER_TOP_GAMES, min_no_ratings 
   naming <- create_name_no_ratings_top_games(min_no_of_ratings = min_no_ratings, top_games = no_top_games)
 
   save_csv(all_last_month_stats,
-    
     filename = paste(RANKING_DATE, naming, sep = "_"),
     filepath = CSV_LAST_MONTHS_FOLDER,
     add_csv_ending = TRUE
